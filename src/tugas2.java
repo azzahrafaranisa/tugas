9b6bdddabc17960887ec4850d96479298a37117a
azzahrafaranisa <146317281+azzahrafaranisa@users.noreply.github.com>
2023-11-02 21:54:54 +0700

    Update and rename ganjilgenap.java to tugas2.java

--- src/tugas2.java
+++ src/tugas2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ganjilgenap {
+public class tugas2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Masukkan sejumlah bilangan: ");
